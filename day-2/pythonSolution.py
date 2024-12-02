f46bcfe3d295cd9d4c3992fa403dcfaf03d1b404
E1Bos <lboscolo68@gmail.com>
2024-12-02 10:12:21 +0000

    added day 2

---
+++ day-2/pythonSolution.py
@@ -0,0 +1,68 @@
+import os
+
+def isSafe(input: list[int]) -> bool:
+    sortedInput = sorted(input)
+    sameDirection = (input == sortedInput) or (input[::-1] == sortedInput)
+
+    isGood = True
+    for i in range(len(input) - 1):
+        difference = abs(input[i] - input[i + 1])
+        if not 1 <= difference <= 3:
+            isGood = False
+            break
+
+    return isGood and sameDirection
+
+
+def fastSolve(lines) -> None:
+    levels = [list(map(int, line.split())) for line in lines]
+
+    part1 = sum(1 for level in levels if isSafe(level))
+    part2 = sum(
+        1
+        for level in levels
+        if any(isSafe(level[:i] + level[i + 1 :]) for i in range(len(level)))
+    )
+
+    # Output
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+
+
+def oldSolve(lines) -> None:
+    levels = [list(map(int, line.split())) for line in lines]
+
+    part1 = 0
+    part2 = 0
+    for level in levels:
+        # Part 1
+        if isSafe(level):
+            part1 += 1
+
+        # Part 2
+        safe = False
+        for badNumber in range(len(levels)):
+            newLevel = level[:badNumber] + level[badNumber + 1 :]
+            if isSafe(newLevel):
+                safe = True
+                break
+
+        if safe:
+            part2 += 1
+
+
+def main():
+    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+    if os.path.exists(input_file_path):
+        with open(input_file_path, "r") as f:
+            lines = f.readlines()
+    else:
+        print("Error: input.txt file does not exist")
+        return
+
+    fastSolve(lines)
+
+
+if __name__ == "__main__":
+    main()
