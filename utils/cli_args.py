a932fe0a25052e682a62fbf1cc945911ee9bfe55
E1Bos <lboscolo68@gmail.com>
2025-01-03 18:55:18 +0100

    feat: enhance Args dataclass with detailed docstring for command line argument handling

--- utils/cli_args.py
+++ utils/cli_args.py
@@ -1,8 +1,31 @@
+"""Module for handling command line arguments."""
+
 from dataclasses import dataclass
 
 
 @dataclass
 class Args:
+    """
+    Class for handling command line arguments.
+
+    Attributes:
+        year (int): The year of the puzzle.
+        day (int): The day of the puzzle.
+        part (int): The part of the puzzle to solve.
+        copy_result (bool): Whether to copy the result to the clipboard.
+        run_all (bool): Whether to run all parts of the puzzle.
+        only_test (bool): Whether to only run the tests.
+        skip_test (bool): Whether to skip the tests.
+        create (bool): Whether to create the necessary files for the specified year and day.
+        timeit (bool): Whether to time the execution of the solution.
+        profile (bool): Whether to profile the execution of the solution.
+        debug (bool): Whether to run the solution in debug mode.
+
+    Properties:
+        year_str (str): The year as a string.
+        day_str (str): The day as a string with 2 digits
+    """
+
     year: int
     day: int
     part: int = 1
@@ -17,8 +40,10 @@ class Args:
 
     @property
     def year_str(self) -> str:
+        """Return the year as a string."""
         return str(self.year)
 
     @property
     def day_str(self) -> str:
+        """Return the day as a string with 2 digits."""
         return str(self.day).zfill(2)
