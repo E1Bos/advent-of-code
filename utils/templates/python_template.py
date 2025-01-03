55b8c223307acf66d0d6b9766944a5657e6b5d5d
E1Bos <lboscolo68@gmail.com>
2025-01-03 18:56:38 +0100

    refactor: clean up whitespace in python_template.py

--- utils/templates/python_template.py
+++ utils/templates/python_template.py
@@ -1,19 +1,16 @@
-from typing import Any
 from utils.solution_base import SolutionBase
-import utils.helper_functions as h
+import utils.helper_functions as h  # noqa
+from typing import Any
 
 
 class Solution(SolutionBase):
     raw_input: bool = False
 
     def parse(self, data: Any) -> Any:
-
         return data
 
     def part1(self, data: Any) -> Any:
-
         return 0
 
     def part2(self, data: Any) -> Any:
-
         return 0
