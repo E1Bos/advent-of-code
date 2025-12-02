fe64455c46f5225a3097e5584a426e1791eadd13
E1Bos <lboscolo68@gmail.com>
2025-12-02 16:18:36 +0000

    refactor: renamed run.py to main.py and formatted with ruff

--- solutions/2025/01.py
+++ solutions/2025/01.py
@@ -1,5 +1,6 @@
 from utils.solution_base import SolutionBase
 
+
 class Solution(SolutionBase):
     raw_input: bool = False
 
@@ -10,13 +11,13 @@ class Solution(SolutionBase):
             if line.startswith("R"):
                 parsed.append(int(line[1::]))
                 continue
-            
+
             parsed.append(-int(line[1::]))
 
         return parsed
 
     def part1(self, data: list[int]) -> int:
-        current_dial: int = 50    
+        current_dial: int = 50
         solution: int = 0
 
         for line in data:
@@ -32,23 +33,23 @@ class Solution(SolutionBase):
 
             if current_dial == 0:
                 solution += 1
-        
+
         return solution
 
     def part2(self, data: list[int]) -> int:
-        current_dial: int = 50    
+        current_dial: int = 50
         solution: int = 0
 
         for line in data:
             next_dial = current_dial + line
             passed_zero = 0
-            
+
             if line > 0:
                 passed_zero = (next_dial // 100) - (current_dial // 100)
             elif line < 0:
                 passed_zero = ((current_dial - 1) // 100) - ((next_dial - 1) // 100)
-            
+
             solution += passed_zero
             current_dial = next_dial % 100
-        
+
         return solution
